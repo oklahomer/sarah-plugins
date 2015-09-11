ea495e5ad65443b6a28a3b0f8972e249ba5036ff
Oklahomer <hagiwara.go@gmail.com>
2015-09-11 23:24:23 +0900

    Use plotly

--- sarah_plugins/barchart.py
+++ sarah_plugins/barchart.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime, timedelta
+from plotly.graph_objs import Data, Scatter
+from plotly.plotly import plotly
+from plotly.tools import FigureFactory
 
 import requests
 import logging
 import json
-from sarah.bot.slack import Slack, SlackMessage, MessageAttachment, \
-    AttachmentField
+from sarah.bot.slack import Slack, SlackMessage, MessageAttachment
 from sarah.bot.values import CommandMessage
 from typing import Dict
 
@@ -38,7 +40,7 @@ class BarchartClient(object):
             raise
 
     def get_history(self, symbol):
-        start_date = datetime.today() - timedelta(days=10)
+        start_date = datetime.today() - timedelta(days=30)
         return self.get("getHistory",
                         {'symbol': symbol,
                          'startDate': start_date.strftime("%Y-%m-%dT00:00:00"),
@@ -51,31 +53,50 @@ def slack_stock_price(msg: CommandMessage, config: Dict):
     if len(msg.text) > 1:
         try:
             response = BarchartClient(config.get('api_key', '')) \
-                .get_history(msg.text)
+                .get_history(msg.text.upper())
         except:
             # API request error
             # Already logged in BarchartClient, so just return error message.
             return "Something went wrong with %s" % msg.text
         else:
             try:
+                open_prices = []
+                high_prices = []
+                low_prices = []
+                close_prices = []
+                dates = []
+                volumes = []
+                for result in response['results']:
+                    open_prices.append(result['open'])
+                    high_prices.append(result['high'])
+                    low_prices.append(result['low'])
+                    close_prices.append(result['close'])
+                    dates.append(result['tradingDay'])
+                    volumes.append(result['volume'])
+
+                candle_graph_url = plotly.plot(
+                    FigureFactory.create_candlestick(open_prices,
+                                                     high_prices,
+                                                     low_prices,
+                                                     close_prices,
+                                                     dates=dates),
+                    filename="barchart/price_" + dates[-1],
+                    vadlidate=False)
+
+                volume_graph_url = plotly.plot(
+                    Data([Scatter(x=dates,
+                                  y=volumes)]))
+
                 attachments = [
-                    MessageAttachment(
-                        fallback="",
-                        title=r['tradingDay'],
-                        fields=[
-                            AttachmentField(title="Open",
-                                            value=r['open'],
-                                            short=True),
-                            AttachmentField(title="Close",
-                                            value=r['close'],
-                                            short=True),
-                            AttachmentField(title="Low",
-                                            value=r['low'],
-                                            short=True),
-                            AttachmentField(title="Volume",
-                                            value=r['volume'],
-                                            short=True)]
-                    ) for r in response['results']]
+                    MessageAttachment(fallback="stock price history",
+                                      title="stock price history",
+                                      title_link=candle_graph_url,
+                                      image_url=candle_graph_url + ".png"),
+                    MessageAttachment(fallback="volume history",
+                                      title="volume history",
+                                      title_link=volume_graph_url,
+                                      image_url=volume_graph_url + ".png")]
+
                 return SlackMessage(
                     text="Stock price history for %s" % msg.text,
                     attachments=attachments)
